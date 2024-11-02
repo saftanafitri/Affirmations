ec6c3ea804b758348d4ba02f156fb4b610b80196
saftanafitri <119521062+saftanafitri@users.noreply.github.com>
2024-11-02 10:46:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.affirmations.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_background = Color(0xFFFFF0F5)
+val md_theme_light_surfaceVariant = Color(0xFFFFE4E1)
+val md_theme_light_onSurfaceVariant = Color(0xFFF764AB)
+
+val md_theme_dark_background = Color(0xFF2E1F26)
+val md_theme_dark_surfaceVariant = Color(0xFF472D35)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD8A0A8)
